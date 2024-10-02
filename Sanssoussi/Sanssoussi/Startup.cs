ec002ff8b3149837468a194bb759804d0a26cd62
Lolomimi99 <laumil99@hotmail.com>
2024-10-02 14:22:04 -0400

    Added CORS + hide server in header
    CORS
    Nom du serveur caché dans localhost mais est-ce que c'est assez..?

--- Sanssoussi/Sanssoussi/Startup.cs
+++ Sanssoussi/Sanssoussi/Startup.cs
@@ -36,6 +36,8 @@ namespace Sanssoussi
                 options.IncludeSubDomains = true;
                 options.MaxAge = TimeSpan.FromDays(60);
             });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
