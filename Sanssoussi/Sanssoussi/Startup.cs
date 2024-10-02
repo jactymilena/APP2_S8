6d8e0c4d1d4e2825343e0027a5572c7681d926e5
aliseeperreault <pera3307@usherbrooke.ca>
2024-10-02 14:14:43 -0400

    Added cookie config

--- Sanssoussi/Sanssoussi/Startup.cs
+++ Sanssoussi/Sanssoussi/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Hosting;
 using static Microsoft.AspNetCore.Http.StatusCodes;
 using System.Net;
 using System;
+using Microsoft.AspNetCore.Http;
 
 namespace Sanssoussi
 {
@@ -36,11 +37,32 @@ namespace Sanssoussi
                 options.IncludeSubDomains = true;
                 options.MaxAge = TimeSpan.FromDays(60);
             });
+
+            services.AddAntiforgery(options =>
+            {
+                options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.SameSite = SameSiteMode.Strict; // SameSite = Strict
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //app.Use(async (context, next) =>
+            //{
+            //    // Supprimer tous les cookies
+            //    foreach (var cookie in context.Request.Cookies.Keys)
+            //    {
+            //        context.Response.Cookies.Delete(cookie);
+            //    }
+
+            //    await next();
+            //});
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
